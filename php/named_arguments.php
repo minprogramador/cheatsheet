427470e9a6945b09e439213f2309db54068b2037
putty <minprogramador@gmail.com>
2022-09-10 13:06:20 -0300

    php named_arguments update

--- php/named_arguments.php
+++ php/named_arguments.php
@@ -12,3 +12,17 @@ function info(string $nome, bool $isAdmin)
 echo info(isAdmin:true, nome:'manoel');
 echo PHP_EOL;
 echo info(nome:'manoel', isAdmin:false);
+echo PHP_EOL . PHP_EOL;
+//...
+
+function runfn($name, $expires)
+{
+    echo ("\nrunfn:\nname: {$name}\nexpires: {$expires}\n");
+}
+
+$data = [
+    'name' => 'test',
+    'expires' => time() + 60 * 60 * 2,
+];
+
+runfn(...$data);
