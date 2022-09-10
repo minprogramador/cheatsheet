21d0399a6eda7640af319bc9f137e299fbf52675
putty <minprogramador@gmail.com>
2022-09-10 11:54:48 -0300

    php match update

--- php/match.php
+++ php/match.php
@@ -18,3 +18,16 @@ if ($meuNome === true) {
 }
 
 var_dump($meuNome);
+
+//--------- Outro modo de uso (lugar de if else) ---------//
+
+$idade = 11;
+
+$resultado = match (true) {
+    $idade >= 65 => 'senhor',
+    $idade >= 25 => 'adulto',
+    $idade >= 18 => 'adolescente',
+    default => 'criança',
+};
+echo PHP_EOL;
+var_dump($resultado);
