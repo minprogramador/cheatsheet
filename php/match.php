262c660b48f7a25a0655a9e961503c39e20751b4
putty <minprogramador@gmail.com>
2022-09-10 13:11:53 -0300

    php match update

--- php/match.php
+++ php/match.php
@@ -18,6 +18,7 @@ if ($meuNome === true) {
 }
 
 var_dump($meuNome);
+echo PHP_EOL;
 
 //--------- Outro modo de uso (lugar de if else) ---------//
 
@@ -29,16 +30,31 @@ $resultado = match (true) {
     $idade >= 18 => 'adolescente',
     default => 'criança',
 };
+
 echo PHP_EOL;
 var_dump($resultado);
 
+$statusCode = 200;
 $message = match ($statusCode) {
     200, 300, 301, 302 => 'combined expressions',
 };
 
 echo PHP_EOL;
-
 echo match (8.0) {
     '8.0' => "Oh no!",
      8.0 => "This is what I expected",
 };
+echo PHP_EOL;
+//................................................//
+function showType($param)
+{
+    return match ($param) {
+        1 => 'Integer',
+        '1' => 'String',
+        true => 'Boolean',
+    };
+}
+
+echo showType(1) . PHP_EOL; // "Integer"
+echo showType('1') . PHP_EOL; // "String"
+echo showType(true) . PHP_EOL; // "Boolean"
