20aea6a0c4e5fdf8949b433f6e1702d1d029e20a
putty <minprogramador@gmail.com>
2022-09-10 09:47:29 -0300

    php lambda update

--- php/lambda.php
+++ php/lambda.php
@@ -8,10 +8,21 @@ $var2 = function ($param = 'lambda 2') {
     return $param;
 };
 
-
-
 echo $var();
 
 echo PHP_EOL;
 
 echo $var2('lambda 2');
+
+/*
+ * Outras formas de chamar a lambda, passando funcao para ela.
+ */
+
+function run($msg)
+{
+    echo $msg();
+}
+
+run(function () {
+    return 'ola mundo';
+});
