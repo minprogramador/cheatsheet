89564bcf77293f39449ad3e407de918a9cd109f5
putty <minprogramador@gmail.com>
2022-09-10 09:52:12 -0300

    php __invoke

---
+++ php/__invoke.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * O método mágico __invoke() foi adicionado na versão 5.3 do PHP,
+ * sendo este executado quando tentamos chamar um objeto como uma função
+ * (bem parecido com uma função variável).
+ */
+
+namespace App;
+
+class Logger
+{
+    public function __invoke($msg)
+    {
+        echo $msg;
+    }
+}
+
+$log = new Logger();
+$log("ola..");
